689559eb8838ce6ea54c5b5e57815d9ac360850b
Emilio Jeldes <emilio.jeldes@gmail.com>
2018-04-24 22:18:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Basic React App using youtube search api
+## Simple react test app using youtube seach api.
+### How to install
+```
+git clone
+cd youtube-search
+```
+
+### Install node dependencies and start dev server
+```
+npm install
+npm start
+```
+### Visit [`http://localhost:8080`](http://localhost:8080) on the browser or via terminal
+```
+start chrome "http://localhost:8080"
+```
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyD7cAQ_Z4pwaar6YpwicGiwE6aJ9KyG8oI';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: API_KEY, term: 'Vue js crash course' }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
